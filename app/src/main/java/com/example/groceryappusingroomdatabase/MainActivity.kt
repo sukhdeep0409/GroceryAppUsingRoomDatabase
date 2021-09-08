7246ae578601c66567391012ce98cd193b10a2f0
SUKHDEEP SINGH <sukhi.aditya.sumit@gmail.com>
2021-09-08 21:24:23 +0530

    schema set up and xml layout completed

--- app/src/main/java/com/example/groceryappusingroomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/groceryappusingroomdatabase/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.groceryappusingroomdatabase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
